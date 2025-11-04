e64339a369c25f11e56c2176f7c904f633be6869
davidomanovic <davidomanovic@gmail.com>
2025-11-04 21:44:30 +0100

    Remove deprecated util module

--- src/quces/utils/__init__.py
+++ src/quces/utils/__init__.py
@@ -1,5 +1,4 @@
 # src/utils/__init__.py
 from .circuit_energy import circuit_energy
-from .ucj_param_energy import ucj_param_energy
 
-__all__ = ["circuit_energy", "ucj_param_energy"]
+__all__ = ["circuit_energy"]
